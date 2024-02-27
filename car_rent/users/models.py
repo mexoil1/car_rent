9f5ff7c691e7723581fcbe43040707eafbbd4a35
Михаил <mxoilslukin@gmail.com>
2024-02-27 14:55:35 +0500

    add user model

--- car_rent/users/models.py
+++ car_rent/users/models.py
@@ -1,2 +1,20 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    '''Model of user'''
+    first_name = models.CharField(
+        max_length=200,
+        verbose_name='Имя')
+    last_name = models.CharField(
+        max_length=200,
+        verbose_name='Фамилия')
+    email = models.EmailField(
+        unique=True,
+        max_length=200,
+        verbose_name='Email')
+    score = models.FloatField(default=5.0, verbose_name='Рейтинг')
+
+    def __str__(self):
+        return f'{self.email} {self.first_name} {self.last_name}'
